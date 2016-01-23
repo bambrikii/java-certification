37febd672a9c7f2a7f8570aef1b6b9896532574e
Ալեքսանդր Առաքելյան <bambrikii@gmail.com>
2016-01-23 23:04:28 +0100

    - typeconversionnullpointerexception.

---
+++ boxing/conversion-nullpointerexception/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    Integer x; // line 2
+    public static void main(String[] args) {
+	new Test().go(5);
+    }
+    void go(Integer i) { // line 6
+	System.out.print( "" + x
+	     +
+	     ++i); // line 7
+
+	System.out.print(x
+	     +
+	     ++i); // line 7
+    }
+}
