1bca34bd97df84ef688b108aa4d14c06fd542846
Ալեքսանդր Առաքելյան <bambrikii@gmail.com>
2016-01-24 00:09:12 +0100

    - assertions: -ea

---
+++ assertions/assertion-catching/Test.java
@@ -0,0 +1,19 @@
+public class Test {
+    public static void main(String[] args) {
+    String[] arr = {"SE","ee","ME"};
+	for(String var : arr) {
+	    try {
+		switch(var) {
+		    case "SE":
+			System.out.println("Standard Edition");
+			break;
+		    case "EE":
+			System.out.println("Enterprise Edition");
+			break;
+		    default: assert false;
+		}
+	    } catch (Exception e) {
+		System.out.println(e.getClass()); }
+	}
+    }
+}
