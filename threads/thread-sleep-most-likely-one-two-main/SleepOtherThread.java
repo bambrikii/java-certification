446e6e2f8f04394ef1d956615a35e260916da3ef
Ալեքսանդր Առաքելյան <bambrikii@gmail.com>
2016-01-31 10:03:53 +0100

    - Thread object .sleep() seems to lead to current thread sleep. Most likely is the result One Two Main.

---
+++ threads/thread-sleep-most-likely-one-two-main/SleepOtherThread.java
@@ -0,0 +1,16 @@
+public class SleepOtherThread {
+public static void main(String[] args) throws InterruptedException {
+Runnable r = new Runnable() {
+public void run() {
+System.out.print(Thread.currentThread().getName());
+}
+};
+Thread t1 = new Thread(r, "One ");
+t1.start();
+t1.sleep(2000);
+Thread t2 = new Thread(r, "Two ");
+t2.start();
+t2.sleep(1000);
+System.out.print("Main ");
+}
+}
