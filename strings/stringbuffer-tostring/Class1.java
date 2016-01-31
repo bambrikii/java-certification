d99e765beb51317476ff796095fe266ae028d12c
Ալեքսանդր Առաքելյան <bambrikii@gmail.com>
2016-01-31 09:39:05 +0100

    - int + stringBufferInstance results to error: bad operand types for binary operator '+'.

---
+++ strings/stringbuffer-tostring/Class1.java
@@ -0,0 +1,6 @@
+public class Class1 {
+    public static void main(String[] args) {
+	StringBuffer b = new StringBuffer("3");
+	System.out.print(5+4+b+2+1); // error: bad operand types for binary operator '+'
+    }
+}
