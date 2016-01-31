87cceec9db6109a7bebff6708e77e7a25ceeec72
Ալեքսանդր Առաքելյան <bambrikii@gmail.com>
2016-01-31 10:01:30 +0100

    - SimpleFileVisitor recursive...

---
+++ nio/simplefilevisitor-files.walkfiletree/SearchApp.java
@@ -0,0 +1,21 @@
+public class SearchApp extends SimpleFileVisitor<Path> {
+    private final PathMatcher matcher;
+    SearchApp() {
+	matcher = FileSystems.getDefault().getPathMatcher("glob:*dat");
+    }
+    void find(Path file){
+	Path name = file.getFileName();
+	if (name != null && matcher.matches(name)){
+	    System.out.println(name);
+	}
+    }
+    public FileVisitResult visitFile (Path file, BasicFileAttributes atr){
+	find(file);
+	return FileVisitResult.CONTINUE;
+    }
+    public static void main(String[] args) throws IOException {
+	SearchApp obj = new SearchApp();
+	Files.walkFileTree(Paths.get("c:/_Workspace/OCP/src/examrest/Company/"), obj);
+    }
+}
+
