116b5b747575298fb35c51b5d8edab1aba2de6f2
Ալեքսանդր Առաքելյան <bambrikii@gmail.com>
2016-03-06 10:43:11 +0100

    - Serialization: If the class does implement Serializable, it will be serialized at runtime despite the fact the property class it is assigned to does not implement Serializable.

--- serialization/serializable-child-reference-can-be-serialized-when-assigned-to-non-serializable-property-declaration/Ser2.java
+++ serialization/serializable-child-reference-can-be-serialized-when-assigned-to-non-serializable-property-declaration/Ser2.java
@@ -1,15 +1,16 @@
 import java.io.*;
 
-public class Ser1 {
+public class Ser2 {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
 	String fileName = "f1.txt";
 	try(FileOutputStream fos = new FileOutputStream(fileName);
 	    ObjectOutputStream oos = new ObjectOutputStream(fos);) {
-	    Cls2 cls2 = new Cls2();
+	    Cls3 cls2 = new Cls3();
 	    cls2.str2 = "str2";
+	    cls2.str3 = "str3";
 	    Cls1 cls1 = new Cls1();
-	    // cls1.cls2 = cls2; // Will throw java.io.NotSerializableException
+	    cls1.cls2 = cls2;
 	    oos.writeObject(cls1);
 	}
 
@@ -19,6 +20,7 @@ public class Ser1 {
 	    System.out.println(cls1);
 	    System.out.println(cls1.cls2);
 	    System.out.println(cls1.cls2 != null ? cls1.cls2.str2 : null);
+	    System.out.println(cls1.cls2 != null ? ((Cls3)cls1.cls2).str3 : null);
 	}
     }
 }
@@ -30,4 +32,9 @@ class Cls1 implements Serializable {
 // Does not implement Serializable! Exception will be thrown at runtime when the the property of this class is declared AND INSTANTIATED 
 class Cls2 {
     public String str2;
+}
+
+// Does implement Serializable. Will be serialized despite the fact the property class it is assigned to does not implement Serializable.
+class Cls3 extends Cls2 implements Serializable {
+    public String str3;
 }
