87f3d44b8cc5e5b91177559c668adfb4b0f0fc61
Ալեքսանդր Առաքելյան <bambrikii@gmail.com>
2017-07-02 11:02:29 +0200

    - impor duplicates are allowed, but - error: a type with the same simple name is already defined by the single-type-import of Date .

---
+++ import-duplicates/ImportDuplicates.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+// import java.sql.Date; // error: a type with the same simple name is already defined by the single-type-import of Date
+import java.util.*;
+import java.util.Date; // Duplicates are allowed
+
+public class ImportDuplicates {
+    public static void main(String[] args) {
+    }
+}
