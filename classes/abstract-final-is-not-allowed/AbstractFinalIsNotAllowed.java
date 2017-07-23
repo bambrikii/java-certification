e6b667705f99fd634f79f9174e7f8364e806e6dd
Ալեքսանդր Առաքելյան <bambrikii@gmail.com>
2017-07-23 11:47:33 +0200

    - abstract + final is now allowed neither for classes, no for methods, - private + abstract is not allowed for methods.

---
+++ classes/abstract-final-is-not-allowed/AbstractFinalIsNotAllowed.java
@@ -0,0 +1,4 @@
+public /*final*/ abstract class AbstractFinalIsNotAllowed { // abstract final is not allowed
+    public /*final*/ abstract void method1(); // abstract final is not allowed
+    private abstract void method2(); // private abstract is not allowed
+}
