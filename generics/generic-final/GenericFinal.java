5e97ab73124afbcdf4d69d055392424cb280e1ce
Alexander Arakelyan <alexander.arakelyan@gmail.com>
2023-05-07 18:34:09 -0400

    final classes can be generic. enums cannot be generic.

---
+++ generics/generic-final/GenericFinal.java
@@ -0,0 +1,11 @@
+public final class GenericFinal<T> {
+    public static void main(String[] args) {
+	new GenericFinal<Integer>();
+    }
+}
+
+
+// Cannot define generic enum
+// enum Enum1<K> { // error: '{' expected
+//     VAL1;
+// }
