2791163dbba62fd72df62474c0487997a3c242ee
Alexander Arakelyan <alexander.arakelyan@gmail.com>
2023-05-06 22:48:15 -0400

    instanceof fails if non-generic declaration is used

--- generics/reifiable-types/ReifiableTypes.java
+++ generics/reifiable-types/ReifiableTypes.java
@@ -2,22 +2,25 @@ public class ReifiableTypes {
     public static void main(String[] args) {
 	Object obj1  = new Type1<Integer>();
 	// error: Object cannot be safely cast to Type1<Integer>
-/*
-	if (obj1 instanceof Type1<Integer>) {
-	    System.out.println("obj1");
-	}
-*/
+	//	if (obj1 instanceof Type1<Integer>) {
+	//	    System.out.println("obj1");
+	//	}
+
 	Type1 obj2  = new Type1<Integer>();
 	// error: Object cannot be safely cast to Type1<Integer>
-/*
-	if (obj2 instanceof Type1<Integer>) {
-	    System.out.println("obj2");
-	}
-*/
+	//	if (obj2 instanceof Type1<Integer>) {
+	//	    System.out.println("obj2");
+	//	}
 
-	Type1<Integer> obj3  = new Type1<Integer>();
-	if (obj3 instanceof Type1<Integer>) {
-	    System.out.println("obj3");
+	Type1<?> obj3  = new Type1<Integer>();
+	// error: Type1<CAP#1> cannot be safely cast to Type1<Integer>
+	//	if (obj3 instanceof Type1<Integer>) {
+	//	    System.out.println("obj3");
+	//	}
+
+	Type1<Integer> obj4  = new Type1<Integer>();
+	if (obj4 instanceof Type1<Integer>) {
+	    System.out.println("obj4");
 	}
     }
 }
